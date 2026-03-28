9a68546b13f14a998e0690689f6a8b00c5d08fab
pmantini <m_pranav@yahoo.com>
2026-03-28 19:48:43 +0000

    Deploying to gh-pages from @ pmantini/profile@16339a38767fe716d50eb69fe1d76baf60300976 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://cs.uh.edu/~pmantini/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://pmantini.github.io/profile/#contact">contact us</a>.'
               }
             ]
           }
